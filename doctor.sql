a8ff6efce1166d2a565a872846015965e6776e8a
tonton <brianmogaka.bm@gmail.com>
2019-04-10 10:58:12 +0300

    Updated to include treatment, cancer type, and removed messages

--- doctor.sql
+++ doctor.sql
@@ -25,12 +25,7 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `doctor` (
-  `id` int(100) NOT NULL,
-  `firstName` varchar(255) NOT NULL,
-  `lastName` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `phoneNo` int(100) NOT NULL,
-  `password` varchar(255) NOT NULL
+
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
@@ -48,8 +43,7 @@ INSERT INTO `doctor` (`id`, `firstName`, `lastName`, `email`, `phone_no`, `passw
 
 CREATE TABLE `nurse` (
   `id` int(100) NOT NULL,
-  `firstName` varchar(255) NOT NULL,
-  `lastName` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `phoneNo` int(100) NOT NULL,
   `password` varchar(255) NOT NULL
@@ -63,31 +57,78 @@ CREATE TABLE `nurse` (
 
 CREATE TABLE `patient` (
   `id` int(100) NOT NULL,
-  `firstName` varchar(255) NOT NULL,
-  `lastName` varchar(255) NOT NULL,
+  `nationalId` int(11) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `phoneNo` int(100) NOT NULL,
+  `phone_no` int(100) NOT NULL,
   `dob` date NOT NULL,
   `gender` varchar(25) NOT NULL,
-  `disease` text NOT NULL,
-  `medicine` text NOT NULL,
+  `address` varchar(255) NOT NULL,
+  `blood_group` varchar(20) NOT NULL,
+  `disease` varchar(255) NOT NULL,
+  `status` varchar(20) NOT NULL DEFAULT 'ACTIVE PATIENT',
   `password` varchar(255) NOT NULL
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- -------------------------------------------------
 
 -- 
--- Table structure for table `messages`
+-- Table structure for table `appointment`
 -- 
 
-CREATE TABLE `messages` (
+CREATE TABLE `appointment` (
   `id` int(100) NOT NULL,
-  `firstName` varchar(25) NOT NULL,
-  `lastName` varchar(25) NOT NULL,
-  `message` text NOT NULL
+  `doctors_id` varchar(20) NOT NULL DEFAULT '1',
+  `app_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `reason` text NOT NULL,
+  `status` varchar(10) NOT NULL DEFAULT 'INACTIVE'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+
+-- 
+-- Table structure for table `treatment`
+-- 
+
+CREATE TABLE `treatment` (
+  `id` int(100) NOT NULL,
+  `treatmentType` varchar(255) NOT NULL
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `treatment`
+--
+
+INSERT INTO `treatment` (`id`, `treatmentType`)VALUES
+(1, 'Chemotherapy'),
+(2, 'Radiotherapy'),
+(3, 'Immunotherapy'),
+(4, 'Hormone Based Chemotherapy'),
+(5, 'Steroids'),
+(6, 'Bone Marrow Stimulant');
+-- -------------------------------------------------
+
+-- 
+-- Table structure for table `cancer`
+-- 
+
+CREATE TABLE `cancer` (
+  `id` int(100) NOT NULL,
+  `cancerType` varchar(255) NOT NUL
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `treatment`
+--
+
+INSERT INTO `cancer` (`id`, `cancerType`)VALUES
+(1, 'Breast Cancer'),
+(2, 'Prostrate Cancer'),
+(3, 'Throat Cancer'),
+(4, 'Lymphoma');
+
+-- -----------------------------------------------
 
 --
 -- Indexes for tables
@@ -112,42 +153,19 @@ ALTER TABLE `patient`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `messages`
+-- Indexes for table `appointment`
 --
-ALTER TABLE `messages`
+ALTER TABLE `appointment`
   ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- Indexes for table `treatment`
 --
-
--- -----------------------------------------------
-
---
--- AUTO_INCREMENT for table `doctor`
---
-ALTER TABLE `doctor`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `treatment`
+  ADD PRIMARY KEY (`id`);
 
 --
--- AUTO_INCREMENT for table `nurse`
+-- Indexes for table `cancer`
 --
-ALTER TABLE `nurse`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-
---
--- AUTO_INCREMENT for table `patient`
---
-ALTER TABLE `patient`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-
---
--- AUTO_INCREMENT for table `messages`
---
-ALTER TABLE `messages`
-  MODIFY `id` int(100) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `cancer`
+  ADD PRIMARY KEY (`id`);
